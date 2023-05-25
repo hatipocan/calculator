ccc6f2325cc9cc2bf0e3085e3166a66b6fa3ab8b
hatipocan <97190685+hatipocan@users.noreply.github.com>
2023-05-25 08:15:59 +0300

    dig2 varissue solved. still issue on operator.

--- script.js
+++ script.js
@@ -27,20 +27,20 @@ function divide(num1, num2) {
 let dig1 = "";
 let dig2 = "";
 let operator = "";
-
+let counter = 0;
 
 //operator function for making the correct mathmetical operation
 
 
 function operate(dig1, dig2, operator) {
     if (operator == "+") {
-        add(dig1, dig2);
+        return add(dig1, dig2);
     } else if (operator == "-") {
-        subtract(dig1, dig2);
+        return subtract(dig1, dig2);
     } else if (operator == "*") {
-        multiply(dig1, dig2);
+        return multiply(dig1, dig2);
     } else if (operator == "/") {
-        divide(dig1,dig2);
+        return divide(dig1,dig2);
     };
 };
 
@@ -48,65 +48,148 @@ function operate(dig1, dig2, operator) {
 
 const displayBoard = document.getElementById("display");
 
-//display value variable
-let displayValue = '';
-
 
 const buttonSeven = document.getElementById("seven");
 buttonSeven.addEventListener("click", () => {
-    if (operator != '') {displayBoard.innerHTML = ""}
-    displayBoard.innerHTML += Number(7);
-    displayValue += '7'});
+    if (operator != '') {
+        if (counter == 1) {counter = 0;
+            dig1 = Number(displayBoard.innerHTML); 
+            displayBoard.innerHTML="";
+                            };};
+    displayBoard.innerHTML += Number(7)
+    ;});
+    
 
 const buttonEight = document.getElementById("eight");
-buttonEight.addEventListener("click", () => {displayBoard.innerHTML += Number(8);
-displayValue += '8'});
+buttonEight.addEventListener("click", () => {
+    if (operator != '') {
+        if (counter == 1) {counter = 0;
+            dig1 = Number(displayBoard.innerHTML); 
+            displayBoard.innerHTML="";
+                            };}; 
+        displayBoard.innerHTML += Number(8);});
 
 const buttonNine = document.getElementById("nine");
-buttonNine.addEventListener("click", () => {displayBoard.innerHTML += Number(9);});
+buttonNine.addEventListener("click", () => {
+    if (operator != '') {
+        if (counter == 1) {counter = 0;
+            dig1 = Number(displayBoard.innerHTML); 
+            displayBoard.innerHTML="";
+                            };}; 
+        displayBoard.innerHTML += Number(9);});
 
 const buttonDivide = document.getElementById("divide");
 buttonDivide.addEventListener("click", () => {
-    operator = "/"; });
+    operator = "/";
+    counter = 1; 
+    if (dig1 != "" && dig2 == "") {
+        dig2 = Number(displayBoard.innerHTML); 
+        dig1 = operate(dig1, dig2, operator);
+        displayBoard.innerHTML = dig1;
+        dig2 = ""; } });
 
 const buttonFour = document.getElementById("four");
-buttonFour.addEventListener("click", () => {displayBoard.innerHTML += Number(4);});
+buttonFour.addEventListener("click", () => {
+    if (operator != '') {
+        if (counter == 1) {counter = 0;
+            dig1 = Number(displayBoard.innerHTML); 
+            displayBoard.innerHTML="";
+                            };};
+        displayBoard.innerHTML += Number(4);});
 
 const buttonFive = document.getElementById("five");
-buttonFive.addEventListener("click", () => {displayBoard.innerHTML += Number(5);});
+buttonFive.addEventListener("click", () => {
+    if (operator != '') {
+        if (counter == 1) {counter = 0;
+            dig1 = Number(displayBoard.innerHTML); 
+            displayBoard.innerHTML="";
+                            };};
+        displayBoard.innerHTML += Number(5);});
 
 const buttonSix = document.getElementById("six");
-buttonSix.addEventListener("click", () => {displayBoard.innerHTML += Number(6);});
+buttonSix.addEventListener("click", () => {
+    if (operator != '') {
+        if (counter == 1) {counter = 0;
+            dig1 = Number(displayBoard.innerHTML); 
+            displayBoard.innerHTML="";
+                            };};
+        displayBoard.innerHTML += Number(6);});
 
 const buttonMultiply = document.getElementById("multiply");
 buttonMultiply.addEventListener("click", () => {
-    operator = "*";});
+    operator = "*";
+    counter = 1;
+    if (dig1 != "" && dig2 == "") {
+        dig2 = Number(displayBoard.innerHTML); 
+        dig1 = operate(dig1, dig2, operator);
+        displayBoard.innerHTML = dig1;
+        dig2 = ""; } });
 
 const buttonOne = document.getElementById("one");
-buttonOne.addEventListener("click", () => {displayBoard.innerHTML += Number(1);});
+buttonOne.addEventListener("click", () => {
+    if (operator != '') {
+        if (counter == 1) {counter = 0;
+            dig1 = Number(displayBoard.innerHTML); 
+            displayBoard.innerHTML="";
+                            };};
+        displayBoard.innerHTML += Number(1);});
 
 const buttonTwo = document.getElementById("two");
-buttonTwo.addEventListener("click", () => {displayBoard.innerHTML += Number(2);});
+buttonTwo.addEventListener("click", () => {
+    if (operator != '') {
+        if (counter == 1) {counter = 0;
+            dig1 = Number(displayBoard.innerHTML); 
+            displayBoard.innerHTML="";
+                            };};
+        displayBoard.innerHTML += Number(2);});
 
 const buttonThree = document.getElementById("three");
-buttonThree.addEventListener("click", () => {displayBoard.innerHTML += Number(3);});
+buttonThree.addEventListener("click", () => {
+    if (operator != '') {
+        if (counter == 1) {counter = 0;
+            dig1 = Number(displayBoard.innerHTML); 
+            displayBoard.innerHTML="";
+                            };};
+        displayBoard.innerHTML += Number(3);});
 
 const buttonSubtract = document.getElementById("subtract");
 buttonSubtract.addEventListener("click", () => {
-    operator = "-";});
+    operator = "-";
+    counter = 1;
+    if (dig1 != "" && dig2 == "") {
+        dig2 = Number(displayBoard.innerHTML); 
+        dig1 = operate(dig1, dig2, operator);
+        displayBoard.innerHTML = dig1;
+        dig2 = ""; } });
 
 const buttonZero = document.getElementById("zero");
-buttonZero.addEventListener("click", () => {displayBoard.innerHTML += Number(0);});
+buttonZero.addEventListener("click", () => {
+    if (operator != '') {
+        if (counter == 1) {counter = 0;
+            dig1 = Number(displayBoard.innerHTML); 
+            displayBoard.innerHTML="";
+                            };};
+        displayBoard.innerHTML += Number(0);});
 
 // Equals button should trigger the operate function . these code block will be changed accordingly
 
 const buttonEquals = document.getElementById("equals");
-buttonEquals.addEventListener("click", operate);
+buttonEquals.addEventListener("click", () => {
+    dig2 = Number(displayBoard.innerHTML);
+    displayBoard.innerHTML = operate(dig1, dig2, operator);
+    counter = 1;
+
+});
 
 const buttonPlus = document.getElementById("plus");
 buttonPlus.addEventListener("click", () => {
-    operator = "+";});
-    //dig1 = displayBoard.innerHTML;});
+    operator = "+";
+    counter = 1;
+    if (dig1 != "" && dig2 == "") {
+        dig2 = Number(displayBoard.innerHTML); 
+        dig1 = operate(dig1, dig2, operator);
+        displayBoard.innerHTML = dig1;
+        dig2 = ""; } });
 
 // Reset and delete buttons
 
@@ -114,12 +197,7 @@ const buttonReset = document.getElementById("reset");
 buttonReset.addEventListener("click", () => {location.reload();});
 
 const buttonDelete = document.getElementById("delete");
-buttonDelete.addEventListener("click", () => {displayBoard.innerHTML = displayBoard.innerHTML.toString().slice(0,-1);
-displayValue = displayValue.slice(0,-1)});
-
-
-
-
+buttonDelete.addEventListener("click", () => {displayBoard.innerHTML = displayBoard.innerHTML.toString().slice(0,-1);});
 
 
 //if operator pressed store the existing number to dig1
